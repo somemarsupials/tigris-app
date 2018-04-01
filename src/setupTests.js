8baf2e0355a9f7e37c88f468b6d4f351fb1d77bc
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-01 09:57:22 +0100

    Install enzyme dependencies and configure tests
    Install react-16 adapter and JSON serialiser. Add
    setupTests script in src/ and add Jest configuration
    to package.json.

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+import Enzyme from 'enzyme';
+import { createSerializer } from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
