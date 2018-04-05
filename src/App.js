19a4f6f1f0c50a567180ad6f613f1f58c5fa687d
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-05 10:58:22 +0300

    Create framework for app in App.js, move tests
    Install React router, embed site links and create
    a route with the HomeContainer component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import { hot } from 'react-hot-loader';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { HomeContainer } from './containers';
+import { SiteHeader } from './components/header';
+
 
 export const App = function (props) {
-  return (<div>Hello world</div>);
+  return (
+    <BrowserRouter>
+      <div>
+        <SiteHeader />
+        <div>
+          <Route exact path="/" component={HomeContainer} />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
