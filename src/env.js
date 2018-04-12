338f73a4aba3bc5eece30f5fb59bfe270d6571a4
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-12 19:20:23 +0100

    Add env.js for configuring environment

---
+++ src/env.js
@@ -0,0 +1,26 @@
+const development = {
+  api: 'http://localhost:5000',
+};
+
+const test = {
+};
+
+const production = {
+};
+
+let env;
+
+switch (process.env.NODE_ENV || 'development') {
+  case 'test':
+    env = test;
+    break;
+  case 'production':
+    env = production;
+    break;
+  case 'development':
+  default:
+    env = development;
+    break;
+};
+
+export default env;
