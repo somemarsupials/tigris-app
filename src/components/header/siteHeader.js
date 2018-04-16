e23fb6d80b473383ee1a59978f33ee78cf606c7d
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-16 14:50:29 +0100

    Add Spotify link to menu bar
    Also apply ESLint to script.

--- src/components/header/siteHeader.js
+++ src/components/header/siteHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TabLink } from '../shared';
 
-export default function (props) {
+export default function () {
   return (
-    <div>
+    <div className="nav">
       <Link to="/">Home</Link>
+      <TabLink show={'Login with Spotify'} href={'http://localhost:5000/auth/spotify/authorise'}/>
     </div>
   );
-};
+}
