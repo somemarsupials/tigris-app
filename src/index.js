0c6dbca263965d99c82668c8cf4cedbaf4354bc1
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-16 14:49:06 +0100

    Setup Redux connection at top level
    Use Provider element to connect app. Also
    use redux-thunk middleware.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import { App } from './App';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-const root = document.getElementById('app');
-ReactDOM.render(<App />, root);
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('app'));
+
